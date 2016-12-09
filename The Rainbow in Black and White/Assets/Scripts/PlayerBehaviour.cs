1577cc507ac2a460b999049a965f81d546617174
Wacken <sebastianwalchi@gmail.com>
2016-12-09 22:50:16 +0100

    Add real Project

---
+++ The Rainbow in Black and White/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+
+    private float side;
+    private float jump;
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        side = Input.GetAxis("Horizontal");
+        jump = Input.GetAxis("Jump");
+	}
+}
